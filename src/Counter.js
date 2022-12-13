fa3f2a2f4cabf9e8f4f710700fda0f77b9b12879
Gabriel <gabriel.alves.rosario@gmail.com>
2022-12-13 10:49:59 +0000

    aula01Hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-class Counter extends React.Component {
+function Counter(props) {
 
-    constructor(props){
-        super(props)
-        this.state = {count: this.props.count}
-        this.add = this.add.bind(this)
+    const [count, setCount] = useState(props.count)
+
+    function add() {
+
+        setCount(count + 1);
+        console.log("Atualziar")
     }
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>Add</button>
+        </div>
+    )
+}
+
+// class Counter extends React.Component {
+
+//     constructor(props){ //1 metodo do ciclo de vida
+//         super(props)
+//         this.state = {count: this.props.count}
+//         this.add = this.add.bind(this)
+//     }
 
     
 
-    add() {
-        //this.setState({count: this.state.count + 1})
-        this.setState((state) => { return {count: state.count +1}})
-    }
+//     add() {
+//         this.setState({count: this.state.count + 1})
+//         this.setState((state) => { return {count: state.count +1}})
+//     }
 
-    render() {
-        return(
-            <div>
-                <h1>Counter: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-            </div>
-        )
-    }
-}
+//     shouldComponentUpdate() {
+//         return true;
+//     }
+
+//     render() { //metodo para atulizar a tela
+//         return(
+//             <div>
+//                 <h1>Counter: {this.state.count}</h1>
+//                 <button onClick={this.add}>Add</button>
+//             </div>
+//         )
+//     }
+// }
+
 export default Counter;
