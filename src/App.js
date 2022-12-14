71293d0ec8d9b7ad262ad626eb65b4488219b2f3
Gabriel <gabriel.alves.rosario@gmail.com>
2022-12-14 16:35:21 +0000

    WillEunMount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import Header from './Header';
 import Counter from './Counter';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Header name="Sara" links={["Sobre", "Comprar", "Contato","Login"]}></Header>   
-      <Counter count={2}></Counter>
-    </div>
-    );
+
+  const [show, setShow] = useState(true)
+
+  let time = 0;
+
+  useEffect(()=>{
+    let timer = setInterval(()=>{//funcao para somar no time a cada 1000(1s)
+      time++;
+      console.log(time)
+      if(time > 5){
+        clearInterval(timer)//funcao para limpar o time
+        setShow(false)
+      }
+    },1000)
+  }, [])
+
+  
+
+  if(show){
+    return (
+      <div>
+        <Header name="Sara" links={["Sobre", "Comprar", "Contato","Login"]}></Header>   
+        <Counter count={2}></Counter>
+      </div>
+      );
+  }else {
+    return(
+      <div>
+        Sem contador
+      </div>
+    )
+  }
 }
 
 export default App;
